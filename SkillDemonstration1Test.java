adb00ef58003456c92ff5fb54a4216b7463fdc27
mariawaseem <97696497+mariawaseem@users.noreply.github.com>
2022-02-04 16:39:41 -0800

    should work now

--- SkillDemonstration1Test.java
+++ SkillDemonstration1Test.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemonstration1Test {
     @Test
     public void multiplication() {
-        assertEquals(4, SkillDemonstration1.multiplication(2,3)); // will fail
+        assertEquals(4, SkillDemonstration1.multiplication(2,2)); // should work now
     }
 }
