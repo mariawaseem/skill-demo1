ad986fabc18414ef013b6e5b41e1df28f7e0625f
mariawaseem <97696497+mariawaseem@users.noreply.github.com>
2022-02-04 16:37:22 -0800

    lib + code that will fail

--- SkillDemonstration1.java
+++ SkillDemonstration1.java
@@ -1,1 +1,5 @@
-public class SkillDemonstration1 {}
+public class SkillDemonstration1 {
+    public static int multiplication(int a, int b) {
+        return a * b;
+    }
+}
---
+++ SkillDemonstration1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemonstration1Test {
+    @Test
+    public void multiplication() {
+        assertEquals(4, SkillDemonstration1.multiplication(2,3)); // will fail
+    }
+}
